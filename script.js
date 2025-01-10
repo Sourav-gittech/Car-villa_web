d1353f20eeafd867d8d6d0172cc5ab5f4dd6fb97
Sourav Mondal <108756502+Sourav-gittech@users.noreply.github.com>
2025-01-10 12:01:08 +0530

    commited

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// hide navbar after clicking
+let navlink = document.querySelectorAll('.nav-link');
+let navbarCollapse = document.querySelector('.navbar-toggler');
+
+navlink.forEach(function (click) {
+    click.addEventListener("click", () => {
+        navbarCollapse.click();
+    });
+});
+
+
+// show or hide scroll button
 let showScroll = () => {
     let scrollBtn = document.querySelector('.scroll-btn');
 
